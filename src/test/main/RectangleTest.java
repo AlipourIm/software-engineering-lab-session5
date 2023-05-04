286bd1a24983add3a970edcbe6a1c2ab5c8e8782
taha7900 <taha7900@gmail.com>
2023-05-04 23:05:21 +0330

    Add tests for Rectangle class

--- src/test/main/RectangleTest.java
+++ src/test/main/RectangleTest.java
@@ -1,20 +1,44 @@
 package main;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
 
 public class RectangleTest {
     @Test
-    public void testSetter() {
+    public void testSetterAndGetter() {
+        Rectangle rc = new Rectangle();
+        rc.setWidth(50);
+        Assert.assertEquals(50, rc.getWidth());
 
+        rc.setHeight(20);
+        Assert.assertEquals(20, rc.getHeight());
     }
 
     @Test
-    public void testGetter() {
-
+    public void testSetterError() {
+        final Rectangle rc = new Rectangle();
+        Assert.assertThrows(new Exception("Height cannot be negative"), new ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                rc.setHeight(-50);
+            }
+        });
     }
 
     @Test
     public void testGetArea() {
+        Rectangle rc = new Rectangle();
+        rc.setHeight(20);
+        rc.setWidth(30);
+        Assert.assertEquals(600, rc.getArea());
+    }
 
+    @Test
+    public void testGetAreaZero() {
+        Rectangle rc = new Rectangle();
+        rc.setHeight(0);
+        rc.setWidth(20);
+        Assert.assertEquals(0, rc.getArea());
     }
 }
