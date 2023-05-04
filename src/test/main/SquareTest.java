bcab7f8f2d387f485554c815f63eaf2804f700d4
taha7900 <taha7900@gmail.com>
2023-05-04 23:07:29 +0330

    Add tests for Square class

--- src/test/main/SquareTest.java
+++ src/test/main/SquareTest.java
@@ -1,15 +1,20 @@
 package main;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SquareTest {
     @Test
-    public void testGetSide() {
-
+    public void testGetterSetter() {
+        Square sq = new Square();
+        sq.setSide(20);
+        Assert.assertEquals(20, sq.getSide());
     }
 
     @Test
     public void testGetArea() {
-
+        Square sq = new Square();
+        sq.setSide(20);
+        Assert.assertEquals(400, sq.getArea());
     }
 }
