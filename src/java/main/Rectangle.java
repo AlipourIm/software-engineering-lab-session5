50d0db016a0d0ebfbadb8a51e267017defea9b27
taha7900 <taha7900@gmail.com>
2023-05-04 23:20:20 +0330

    Add getters and setters for Rectangle class

--- src/java/main/Rectangle.java
+++ src/java/main/Rectangle.java
@@ -13,4 +13,20 @@ public class Rectangle extends Shape{
     double getArea() {
         return this.width * this.height;
     }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        this.height = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
